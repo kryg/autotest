91550ac8d6845d9f56f536908aa4d0b5f9ee2bc1
kryg <vskr@alytics-team.com>
2016-06-20 18:35:02 +0300

    In progress autotest createProject

--- djangoProject/testProject/register.feature
+++ djangoProject/testProject/register.feature
@@ -4,14 +4,17 @@ Feature: Register in testing.alytics.ru
   Scenario: Register
     Given Visit the url "https://testing.alytics.ru/"
       When Click ".js-to-register"
-      And Fill "#name" with "autotest"
+      And Fill "#name" with "autotest111"
       And Fill "#company" with "KVautotest"
       And Fill "#site" with "autotest.com"
       And Fill "#work" with "developer"
-      And Fill "#email" with "autotest@developer.com"
+      And Fill "#email" with "autot1est1@develope11r.com"
       And Fill "#phone" with "123456"
       And Fill "#password1" with "qwerty"
       And Fill "#password2" with "qwerty"
       And Fill "#promo_code" with "TEST PROMO"
       And Click the button ".js-reg-submit"
       Then See the queue message
+      And User should be present in the database:
+          | autotest111                |
+          | autot1est1@develope11r.com  |
--- djangoProject/testProject/step.py
+++ djangoProject/testProject/step.py
@@ -4,7 +4,7 @@
 from lettuce import step, world
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+from selenium.webdriver.common.by import By
 
 @step(r'Visit the url "(.*)"')
 def open_url(step, url):
@@ -29,9 +29,41 @@ def click_element(step, selector):
     button.click()
 
 
-# @step('See the queue message')
-# def queue_message(step):
-#     element = WebDriverWait(world.browser, 10).until(
-#         EC.presence_of_element_located((By.CLASS_NAME, "queue-text"))
-#     )
-#     assert element.text == u'Ваш номер в очереди:'
+@step('See the queue message')
+def queue_message(step):
+    element = WebDriverWait(world.browser, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "queue-text"))
+    )
+    assert element.text == u'Ваш номер в очереди:'
+
+
+@step('should be present in the database')
+def queue_message(step):
+    element = WebDriverWait(world.browser, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "queue-text"))
+    )
+    assert element.text == u'Ваш номер в очереди:'
+
+
+@step('Wait disappearance "(.*)"')
+def wait_for(step, selector):
+    WebDriverWait(world.browser, 200).until_not(
+        EC.presence_of_element_located((By.CSS_SELECTOR, selector))
+    )
+
+
+@step('Wait appearance "(.*)"')
+def wait_for(step, selector):
+    WebDriverWait(world.browser, 200).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, selector))
+    )
+
+
+@step('Switch to new window opened')
+def wait_for(step):
+    for handle in world.browser.window_handles:
+        world.browser.switch_to_window(handle)
+    # world.browser.switch_to_window(windowName)
+    # element = world.browser.find_element_by_name("frameName")
+    # WebDriverWait(world.browser, 200).until(
+    #     world.browser.switch_to_window("frameName"))
